e3112a420d47e3b450409446ddb11f080830d970
fogelmariya <fogelmariya@mail.ru>
2018-06-19 11:17:20 +0300

    todo done

--- src/test/java/hw7/MetalsColorsPageTest.java
+++ src/test/java/hw7/MetalsColorsPageTest.java
@@ -1,30 +1,30 @@
 package hw7;
 
-import base.TestInit;
+import base.InitTest;
 import entities.MetalsColorsData;
 import entities.User;
-import enums.Users;
+import enums.HeaderMenuItems;
 import org.testng.annotations.Test;
 
 import static site.JdiSite.*;
 
 
 // TODO are you tricking me ? Where is .gitignore file ?
-public class MetalsColorsPageTest extends TestInit {
+public class MetalsColorsPageTest extends InitTest {
 
     @Test
     public void metalsColorsDefaultTest() {
         //1 Login on JDI site as User
         homePage.open();
         // TODO what is the reason of new instance creation ?? Is Users.PITER_CHAILOVSKII not enough ?
-        login(new User(Users.PITER_CHAILOVSKII.login, Users.PITER_CHAILOVSKII.password));
+        login(new User());
         homePage.checkOpened();
 
         // TODO What would you do in case if you should open all of the pages ?
         // TODO Wrote certain method for each of them ?
         // TODO it will be better to write only ONE parametrised method for open whatever page we want
         //2 Open Metals & Colors page by Header menu
-        openMetalsColorsPage();
+        openPage(HeaderMenuItems.METALS_COLORS.item);
         metalsColorsPage.checkOpened();
 
         //3 Fill form Metals & Colors by data below:
